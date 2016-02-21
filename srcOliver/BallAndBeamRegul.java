8dd847cacebaa1022421a963c6871830b6e94b04
elt12opa <elt12opa@student.lu.se>
2016-02-21 22:34:03 +0100

    min src

---
+++ srcOliver/BallAndBeamRegul.java
@@ -0,0 +1,77 @@
+import SimEnvironment.*;
+
+// BallAndBeamRegul class to be written by you
+public class BallAndBeamRegul extends Thread {
+	// Constructor
+	  private AnalogSource analogInAngle;
+	  private AnalogSource analogInPosition;
+	  private AnalogSink analogOut;
+	  private AnalogSink analogRef;
+	private ReferenceGenerator referenceGenerator;
+	private PID controller;
+
+	
+	
+	//Define min and max control output
+	private double uMin = -10.0;
+	private double uMax = 10.0;
+	
+	public BallAndBeamRegul(ReferenceGenerator refgen, BallAndBeam bb, int priority){
+		  // In Constructor
+		  analogInPosition = bb.getSource(0);
+		  analogInAngle = bb.getSource(1);
+		  analogOut = bb.getSink(0);
+		  analogRef = bb.getSink(1);
+		  referenceGenerator = refgen;
+		  controller = new PID("PID");
+		  setPriority(priority);
+		
+	}
+	
+	public void run(){
+		long t = System.currentTimeMillis();
+		while (true) {
+			// Read inputs
+			double y = analogRef.get();
+			double ref = referenceGenerator.getRef();
+			
+			synchronized (controller) { // To avoid parameter changes in between
+				// Compute control signal
+				double u = limit(controller.calculateOutput(y, ref), uMin, uMax);
+				
+				// Set output
+				analogOut.set(u);
+				analogInPosition.set(u);
+				analogInAngle.set(u);
+				
+				// Update state
+				controller.updateState(u);
+			}
+			analogRef.set(ref); // Only for the plotter animation
+			
+			t = t + controller.getHMillis();
+			long duration = t - System.currentTimeMillis();
+			if (duration > 0) {
+				try {
+					sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
+	}
+
+		//Saturate output at limits
+	private double limit(double u, double umin, double umax) {
+		if (u < umin) {
+			u = umin;
+		} else if (u > umax) {
+			u = umax;
+		} 
+		return u;
+	}
+	
+
+	
+}
---
+++ srcOliver/PID.java
@@ -0,0 +1,74 @@
+// PID class to be written by you
+public class PID {
+	// Current PID parameters
+	private PIDParameters p;
+	private double I, D; // Integrator state
+	private double v; // Desired control signal
+	private double e; // Current control error
+	private double ad, bd;
+	private double yOld, newY;
+	
+	
+	// Constructor
+	public PID(String name){
+		PIDParameters p = new PIDParameters();
+		  p.Beta = 1.0;
+		  p.H = 0.1;
+		  p.integratorOn = false;
+		  p.K = 1;
+		  p.Ti = 0.0;
+		  p.Tr = 10.0;
+		  p.N = 5;
+		  new PIDGUI(this, p, name);
+		  setParameters(p);
+		  ad = p.Td / (p.Td + p.N*p.H);
+		  bd = p.K*ad*p.N;
+		  this.yOld = 0;
+		  this.newY = 0;
+		  this.I = 0.0;
+		  this.v = 0.0;
+		  this.e = 0.0;
+	}
+	
+	// Calculates the control signal v.
+	// Called from BallAndBeamRegul.
+	public synchronized double calculateOutput(double y, double yref){
+		newY = y;
+		e = yref - y;
+		D = ad*D - bd*(y - yOld);
+		v = p.K*(p.Beta*yref - y) + I + D;
+		return v;
+	}
+	
+	// Updates the controller state.
+	// Should use tracking-based anti-windup
+	// Called from BallAndBeamRegul.
+	public synchronized void updateState(double u){
+		  if (p.integratorOn) {
+			  I = I + (p.K * p.H / p.Ti) * e + (p.H / p.Tr) * (u - v);
+			  yOld = newY;
+		  } else {
+			  I = 0.0;
+		  }
+		
+		
+		
+	}
+	
+	// Returns the sampling interval expressed as a long.
+	// Explicit type casting needed.
+	public synchronized long getHMillis(){
+		return (long)(p.H*1000);
+	}
+	
+	// Sets the PIDParameters.
+	// Called from PIDGUI.
+	// Must clone newParameters.
+	public synchronized void setParameters(PIDParameters newParameters){
+		p = (PIDParameters)newParameters.clone();
+		if (!p.integratorOn) {
+			I = 0.0;
+		}
+	  
+	}
+}
